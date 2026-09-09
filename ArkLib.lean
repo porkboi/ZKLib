19d3841cbffb90c7e63d8b64f26c2a81578b4e33
Quang Dao <qvd@andrew.cmu.edu>
2026-09-09 03:00:23 +0000

    feat(Polynomial): bound both bivariate factor degree sums (#873)

--- ArkLib.lean
+++ ArkLib.lean
@@ -258,6 +258,7 @@ import ArkLib.Data.MvPolynomial.RestrictDegree
 import ArkLib.Data.MvPolynomial.RestrictDegreeVar
 import ArkLib.Data.MvPolynomial.SchwartzZippelCounting
 import ArkLib.Data.Polynomial.Bivariate
+import ArkLib.Data.Polynomial.BivariateFactorDegrees
 import ArkLib.Data.Polynomial.ClassicalWronskian
 import ArkLib.Data.Polynomial.FoldedWronskian
 import ArkLib.Data.Polynomial.FoldingPolynomial
