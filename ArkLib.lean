9de9373661713fca77abc9fd708150e0c44efeb9
Quang Dao <qvd@andrew.cmu.edu>
2026-09-09 03:00:27 +0000

    feat(Polynomial): generalize resultant degree bounds and reuse native proof (#878)

--- ArkLib.lean
+++ ArkLib.lean
@@ -277,6 +277,7 @@ import ArkLib.Data.Polynomial.RationalFunctions.HenselNumerators.Weight
 import ArkLib.Data.Polynomial.RationalFunctions.Lifts
 import ArkLib.Data.Polynomial.RationalFunctions.RationalRootVanishing
 import ArkLib.Data.Polynomial.RationalFunctions.Weight
+import ArkLib.Data.Polynomial.ResultantDegree
 import ArkLib.Data.Polynomial.SplitFold
 import ArkLib.Data.Polynomial.SymbolicInterpolationParameters
 import ArkLib.Data.Polynomial.SymbolicInterpolationSupport
